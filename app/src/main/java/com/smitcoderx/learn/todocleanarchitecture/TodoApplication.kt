604f4fc323d87d53c6831c79760a9629bb49558d
SmitCoderX <anirudh16june@gmail.com>
2024-02-28 10:30:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/smitcoderx/learn/todocleanarchitecture/TodoApplication.kt
@@ -0,0 +1,9 @@
+package com.smitcoderx.learn.todocleanarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApplication : Application() {
+
+}
